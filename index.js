28e0c711c06477194b23600f7a5d1923afc8ae9c
Stevennius Chandra <stevenniuschandra@gmail.com>
2024-06-15 19:38:38 +0700

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const { Telegraf, Telegram } = require('telegraf')
+const app = express()
+require('dotenv').config()
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const tele = new Telegram(process.env.BOT_TOKEN)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/send", async (req, res) => {
+  try{
+    if(!req.body.data) throw "Data property not found!"
+    tele.sendMessage(process.env.SENDTO_ID, req.body.data)
+    res.json({
+      status: "oke"
+    })
+  }catch(error){
+    res.json({
+      status: "error",
+      error
+    })
+  }
+})
+
+bot.launch();
+app.listen(3000, () => console.log("App Listening to 3000"))
